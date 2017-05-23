df7daa3b17c5cf1c05dfc9cc75806215a1fa8bcd
Attila Gal <Attila_Gal@epam.com>
2017-05-23 14:46:47 +0200

    Test introduced.

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,8 +1,16 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import App from './App.test';
-//
-//it('renders without crashing', () => {
-//  const div = document.createElement('div');
-//  ReactDOM.render(<App />, div);
-//});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import todoAppReducer from '../../reducers/reducerApp';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux';
+
+let store = createStore(todoAppReducer);
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>, div);
+});
